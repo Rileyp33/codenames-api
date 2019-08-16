178dc2ad7d7e7b5cc02e5a05cc97b589fc6423ce
Riley Pratt <rpratt@adkadmins-MBP.localdomain>
2019-08-16 12:22:23 -0400

    Redis config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,11 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.web_socket_server_url = "wss://codenames-api-rp.herokuapp.com/cable"
+
+  config.action_cable.disable_request_forgery_protection = true
+
+  # Rails.application.config.action_cable.allowed_request_origins = ['http://192.168.1.246:8081', nil]
+
 end
